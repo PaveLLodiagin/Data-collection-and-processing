906b3e1bd999e837761059fa9b59099f97a2bba9
Lodiagin <wonderwaffe@mail.ru>
2021-08-01 09:44:30 +0300

    Исходный файл

--- Lesson 02/HH.py
+++ Lesson 02/HH.py
@@ -12,7 +12,7 @@ url = 'https://hh.ru'
 params = {'fromSearchLine':'true',
           'st':'searchVacancy',
           'text':'Data science',
-          'page':''}
+          'page': 0 }
 
 # укажем заголовки, User-Agent - не будет лишним
 headers={'User-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'}
@@ -30,58 +30,62 @@ block_list = dom.find_all('div', {'class':'vacancy-serp-item'})
 # pprint(len(block_list)) - можно проверить если все возвращает какую то длинну, то идём дальше
 
 #создадим пустой список куда закинем данные (Название вакансии, ссылку, и зарплату)
+
+next_link = dom.find('a', {'data-qa':'pager-next'})['href']
 vacancy = []
+for params['page'] in range(0, params['page']):
 
+    while next_link != None:
+        for block in block_list:
+            block_data = {} # создаем каждый раз над каждой итерацией отдельный словарь
+                        # когда дойдем до конца у нас будет список из 20 словарей по каждой вакансии.
 
-for block in block_list:
-    block_data = {} # создаем каждый раз над каждой итерацией отдельный словарь
-                # когда дойдем до конца у нас будет список из 50 словарей по каждой вакансии.
+                    #Имя - в имени нам нужно получить текстовую составляющию, по этому в конце getText()
+            block_name = block.find('a', {'data-qa':'vacancy-serp__vacancy-title'}).getText()
 
-    #Имя - в имени нам нужно получить текстовую составляющию, по этому в конце getText()
-    block_name = block.find('a', {'data-qa':'vacancy-serp__vacancy-title'}).getText()
+                    #Ссылка - с полученным э-ом надо обращаться как со словарем, допишем в конце ['href'], и допишем url, для кликабельности.
+            block_link = block.find('a', {'data-qa':'vacancy-serp__vacancy-title'})['href']
 
-    #Ссылка - с полученным э-ом надо обращаться как со словарем, допишем в конце ['href'], и допишем url, для кликабельности.
-    block_link = block.find('a', {'data-qa':'vacancy-serp__vacancy-title'})['href']
+                    #Зарплата ,достаем сначало блок
+            block_salary = block.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
 
-    #Зарплата ,достаем сначало блок
-    block_salary = block.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
+            if not block_salary: # исключаем отсутствие
+                salary_min = None
+                salary_max = None
+                salary_interval = None
 
-    if not block_salary: # исключаем отсутствие
-        salary_min = None
-        salary_max = None
-        salary_interval = None
+            else:
+                block_salary=block_salary.getText().replace(u'\xa0', u' ')
 
+                block_salary = re.split(r'\s|-', block_salary)
+
+            if block_salary[0] == 'до':
+                salary_min = None
+                salary_max = int(block_salary[1]+block_salary[2])
+                if block_salary[3] == 'USD':
+                    salary_max = salary_max * 70
+
+            elif block_salary[0] == 'от':
+                salary_min = int(block_salary[1]+block_salary[2])
+                if block_salary[3] == 'USD':
+                    salary_min = salary_min * 70
+                salary_max = None
+
+            else:
+                salary_min = int(block_salary[0]+block_salary[1])
+                if block_salary[5] == 'USD':
+                    salary_min = salary_min * 70
+                salary_max = int(block_salary[3] + block_salary[4])
+                if block_salary[5] == 'USD':
+                    salary_max = salary_max * 70
+
+                    # сохраняем serial_data
+                block_data['name'] = block_name
+                block_data['link'] = block_link
+                block_data['salary_min'] = salary_min
+                block_data['salary_max'] = salary_max
+            vacancy.append(block_data) # добавляем наше все в начальный список
+        params['page'] += 1
     else:
-        block_salary=block_salary.getText().replace(u'\xa0', u' ')
-
-        block_salary = re.split(r'\s|-', block_salary)
-
-        if block_salary[0] == 'до':
-            salary_min = None
-            salary_max = int(block_salary[1]+block_salary[2])
-            if block_salary[3] == 'USD':
-                salary_max = salary_max * 70
-
-        elif block_salary[0] == 'от':
-            salary_min = int(block_salary[1]+block_salary[2])
-            if block_salary[3] == 'USD':
-                salary_min = salary_min * 70
-            salary_max = None
-
-        else:
-            salary_min = int(block_salary[0]+block_salary[1])
-            if block_salary[5] == 'USD':
-                salary_min = salary_min * 70
-            salary_max = int(block_salary[3] + block_salary[4])
-            if block_salary[5] == 'USD':
-                salary_max = salary_max * 70
-
-# сохраняем serial_data
-    block_data['name'] = block_name
-    block_data['link'] = block_link
-    block_data['salary_min'] = salary_min
-    block_data['salary_max'] = salary_max
-
-    vacancy.append(block_data) # добавляем наше все в начальный список
-
-pprint(vacancy)
+        break
+print(next_link)
